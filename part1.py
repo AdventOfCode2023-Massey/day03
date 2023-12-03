c10a8ae169ec039d1f6efb48e3ed607f1b7c8a76
Bart Massey <bart.massey@gmail.com>
2023-12-03 02:30:56 -0800

    fixed a thing, more to do

--- part1.py
+++ part1.py
@@ -8,18 +8,16 @@ def main(filename):
     with open(filename, 'r') as file:
         schematic = [list(line.strip()) for line in file]
 
-    symbols = set(['*', '#', '+', '$'])
     total = 0
 
     for i in range(len(schematic)):
         for j in range(len(schematic[i])):
-            if schematic[i][j].isdigit():
+            if schematic[i][j].isdigit() == False and schematic[i][j] != '.':
                 for di in [-1, 0, 1]:
                     for dj in [-1, 0, 1]:
                         ni, nj = i + di, j + dj
-                        if 0 <= ni < len(schematic) and 0 <= nj < len(schematic[i]) and schematic[ni][nj] in symbols:
-                            total += int(schematic[i][j])
-                            break
+                        if 0 <= ni < len(schematic) and 0 <= nj < len(schematic[i]) and schematic[ni][nj].isdigit():
+                            total += int(schematic[ni][nj])
     print(total)
 
 if __name__ == "__main__":
