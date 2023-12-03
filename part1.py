58449a578759e16ee21d9baf4b146326507c2272
Bart Massey <bart.massey@gmail.com>
2023-12-03 02:42:17 -0800

    got program to pass test input; failed real one

--- part1.py
+++ part1.py
@@ -7,29 +7,27 @@ import re
 
 def main(filename):
     with open(filename, 'r') as file:
-        schematic = [line.strip() for line in file]
+        schematic = [list(line.strip()) for line in file]
 
     numbers = {}
     for i in range(len(schematic)):
-        for match in re.finditer(r'\d+', schematic[i]):
+        line = ''.join(schematic[i])
+        for match in re.finditer(r'\d+', line):
             for j in range(match.start(), match.end()):
                 numbers[(i, j)] = int(match.group())
 
-    symbols = set(['*', '#', '+', '$'])
     total = 0
+    counted = set()
 
     for i in range(len(schematic)):
         for j in range(len(schematic[i])):
-            if schematic[i][j] in symbols:
+            if schematic[i][j].isdigit() == False and schematic[i][j] != '.':
                 for di in [-1, 0, 1]:
                     for dj in [-1, 0, 1]:
                         ni, nj = i + di, j + dj
-                        if (ni, nj) in numbers:
+                        if (ni, nj) in numbers and numbers[(ni, nj)] not in counted:
                             total += numbers[(ni, nj)]
-                            break
-                    else:
-                        continue
-                    break
+                            counted.add(numbers[(ni, nj)])
 
     print(total)
 
